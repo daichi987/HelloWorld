28c284961f7e19cbcbf1b4b7ab438c3d30ffd4c7
daichi987 <masashi.y1028@gmail.com>
2019-06-28 22:45:44 +0900

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Mika on 2019/06/28.
+//  Copyright © 2019 daichi987. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
